1f8828f75dff4660ef3df9e1914f33989680b1b8
jonhayjr <jonhayjr@gmail.com>
2021-08-03 19:26:39 -0700

    Added delete functionality

--- client/src/NoteDetail/NoteDetail.js
+++ client/src/NoteDetail/NoteDetail.js
@@ -20,26 +20,39 @@ const NoteDetail = () => {
         )
     }, [notes])
 
+    const handleDelete = (e) => {
+        //Prevent default behavior
+        e.preventDefault();
+
+        //Get id
+        const id = e.target.getAttribute('data-index');
+
+        //Send delete request to api
+        axios.delete(`${Config.apiBaseUrl}/notes/${id}`);
+    }
+
     const noteElements = notes ?
     notes.map((note, index) => 
         ( 
-            <div className="card" key={index}>
+            <div className="card" key={note.id}>
                 <div className="card-header">
                     <p>{Moment(note.date).format('MMMM Do YYYY')}</p>
                 </div>
                 <ul className="list-group list-group-flush">
-                    <li className="list-group-item">Diet: {note.diet}</li>
+                    <li className="list-group-item" value={note.diet}></li>
                     <li className="list-group-item">Mood: {note.mood}</li>
                     <li className="list-group-item">Symptoms: {note.symptoms}</li>
                     <li className="list-group-item">Exercise: {note.exercise}</li>
                 </ul>
+
+                <button data-index={note.id} type="submit" className="btn btn-primary w-50 mt-2" onClick={handleDelete}>Delete</button>
             </div>
         )
     )
     : '';
 
     return (
-        <div className="note-container">
+        <div className="note-container mb-5">
           {
              noteElements
           }
